01a02b2fcf74f4dacd67e8e00d980a517e45df20
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-25 06:10:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+// var katzDeliLine = []
+
+// takeANumber
+// 1) adds a person to the line
+// 2) appends the person the end of the line if there are already people on it
+// 3) properly handles multiple people being added
+
+function takeANumber(array, name) {
+  array.push(name)
+  return (`Welcome, ${name}`. You are number ${(array.length - 1)} in the line.)
+  
+}
+
+
+// nowServing
+// 4) returns "There is nobody waiting to be served!" when no one is on line
+// 5) returns an announcement about the person it is serving, and shifts the line
+
+
+// currentLine
+// 6) returns "The line is currently empty." if no one is in line
+// 7) says who is in line when there are people waiting
+// 8) returns a different response programmatically given a different array of names
