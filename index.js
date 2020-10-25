87a38bdaaf6d8af19d0f5baf561ca15cd8409b8e
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-25 07:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ function currentLine(array) {
   if (array.length === 0) {
     return ('The line is currently empty.')
   } else {
-    return (`The line is currently: ${array.keys() + array()}`)
+    var line = []
+    for (let i = ; i < array.length; i++) {
+      line.push(` + [i+1] + ${array(i)}`)
+    }
+    return (`The line is currently: ${line}`)
   }
 }
